e77ed9db29394fad68e678d79ed9c31798e2c901
GeroVeni <geroveni@gmail.com>
2019-05-30 02:29:53 +0100

    Change test_parse to conform to pep8

--- logic-sim/test_parse.py
+++ logic-sim/test_parse.py
@@ -8,6 +8,7 @@ from network import Network
 from monitors import Monitors
 from parse import Parser
 
+
 @pytest.fixture
 def new_parser(filename):
     """Return a new instance of the Parser class and
@@ -26,6 +27,7 @@ def new_parser(filename):
     monitors = Monitors(names, devices, network)
     return Parser(names, devices, network, monitors, scanner)
 
+
 @pytest.mark.parametrize("filename, success, error_list", [
     # Correct definition files
     ("test_spec_1.txt", True, []),
@@ -36,32 +38,43 @@ def new_parser(filename):
     ("test_semantic_device_value.txt", False, [
         Parser.DEVICE_VALUE_ERROR] * 8 + [Parser.SYNTAX_ERROR]),
     # TODO Change this test to use Parser.INVALID_PORT_ERROR
-    ("test_semantic_invalid_device_output.txt", False, [Parser.INVALID_DEVICE_OUTPUT_ERROR]),
+    ("test_semantic_invalid_device_output.txt", False,
+        [Parser.INVALID_DEVICE_OUTPUT_ERROR]),
     ("test_semantic_invalid_port.txt", False, [Parser.INVALID_PORT_ERROR]),
-    ("test_semantic_keyword.txt", False, [Parser.KEYWORD_ERROR] * 2 + [Parser.SYNTAX_ERROR]),
+    ("test_semantic_keyword.txt", False, [Parser.KEYWORD_ERROR] * 2 + \
+        [Parser.SYNTAX_ERROR]),
     ("test_semantic_missing_inputs.txt", False, [Parser.MISSING_INPUTS_ERROR]),
     ("test_semantic_mon_input.txt", False, [Parser.MONITOR_INPUT_ERROR]),
     ("test_semantic_not_gate.txt", False, [Parser.NOT_GATE_ERROR]),
-    ("test_semantic_out_of_bounds_inputs.txt", False, [Parser.OUT_OF_BOUND_INPUTS_ERROR]),
+    ("test_semantic_out_of_bounds_inputs.txt", False,
+        [Parser.OUT_OF_BOUND_INPUTS_ERROR]),
     ("test_semantic_repeated_identifier.txt", False, [
         Parser.REPEATED_IDENTIFIER_ERROR] * 2 + [Parser.SYNTAX_ERROR]),
     ("test_semantic_repeated_input.txt", False, [Parser.REPEATED_INPUT_ERROR]),
-    ("test_semantic_repeated_monitor.txt", False, [Parser.REPEATED_MONITOR_ERROR]),
-    ("test_semantic_undefined_device.txt", False, [Parser.UNDEFINED_DEVICE_ERROR]),
-    ("test_semantic_unmatched_input_output.txt", False, [Parser.UNMATCHED_INPUT_OUTPUT_ERROR]),
+    ("test_semantic_repeated_monitor.txt", False,
+        [Parser.REPEATED_MONITOR_ERROR]),
+    ("test_semantic_undefined_device.txt", False,
+        [Parser.UNDEFINED_DEVICE_ERROR]),
+    ("test_semantic_unmatched_input_output.txt", False,
+        [Parser.UNMATCHED_INPUT_OUTPUT_ERROR]),
     # Syntax error files
     ("test_syntax_con_def_invalid_symbol.txt", False, [Parser.SYNTAX_ERROR]),
     ("test_syntax_con_def_missing_comma.txt", False, [Parser.SYNTAX_ERROR]),
-    ("test_syntax_con_def_missing_semicolon.txt", False, [Parser.SYNTAX_ERROR]),
+    ("test_syntax_con_def_missing_semicolon.txt", False,
+        [Parser.SYNTAX_ERROR]),
     ("test_syntax_dev_def_missing_comma.txt", False, [Parser.SYNTAX_ERROR]),
-    ("test_syntax_dev_def_missing_parenthesis.txt", False, [Parser.SYNTAX_ERROR] * 2),
-    ("test_syntax_dev_def_missing_semicolon.txt", False, [Parser.SYNTAX_ERROR]),
+    ("test_syntax_dev_def_missing_parenthesis.txt", False,
+        [Parser.SYNTAX_ERROR] * 2),
+    ("test_syntax_dev_def_missing_semicolon.txt", False,
+        [Parser.SYNTAX_ERROR]),
     ("test_syntax_invalid_argument.txt", False, [Parser.SYNTAX_ERROR]),
     ("test_syntax_invalid_device.txt", False, [Parser.SYNTAX_ERROR]),
     ("test_syntax_invalid_names.txt", False, [
         Parser.SYNTAX_ERROR, Parser.SYNTAX_ERROR]),
-    ("test_syntax_invalid_port.txt", False, [Parser.SYNTAX_ERROR, Parser.SYNTAX_ERROR]),
-    ("test_syntax_invalid_signal.txt", False, [Parser.SYNTAX_ERROR, Parser.SYNTAX_ERROR]),
+    ("test_syntax_invalid_port.txt", False,
+        [Parser.SYNTAX_ERROR, Parser.SYNTAX_ERROR]),
+    ("test_syntax_invalid_signal.txt", False,
+        [Parser.SYNTAX_ERROR, Parser.SYNTAX_ERROR]),
     ("test_syntax_invalid_symbol.txt", False, [Parser.SYNTAX_ERROR]),
     ("test_syntax_missing_cons_1.txt", False, [Parser.SYNTAX_ERROR]),
     ("test_syntax_missing_cons_2.txt", False, [Parser.SYNTAX_ERROR]),
@@ -73,8 +86,10 @@ def new_parser(filename):
     ("test_syntax_missing_mons_2.txt", False, [Parser.SYNTAX_ERROR] * 2),
     ("test_syntax_missing_mons_3.txt", False, [Parser.SYNTAX_ERROR]),
     ("test_syntax_mon_def_missing_comma.txt", False, [Parser.SYNTAX_ERROR]),
-    ("test_syntax_mon_def_missing_semicolon.txt", False, [Parser.SYNTAX_ERROR]),
-    ("test_syntax_no_devices.txt", False, [Parser.KEYWORD_ERROR, Parser.SYNTAX_ERROR]),
+    ("test_syntax_mon_def_missing_semicolon.txt", False,
+        [Parser.SYNTAX_ERROR]),
+    ("test_syntax_no_devices.txt", False,
+        [Parser.KEYWORD_ERROR, Parser.SYNTAX_ERROR]),
     ("test_syntax_wrong_symbol.txt", False, [Parser.SYNTAX_ERROR]),
 ])
 def test_parse(new_parser, success, error_list):
